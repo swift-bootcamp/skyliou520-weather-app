3b5a1f7b14f6ea4464d6931305410ef2c956e972
skyliou520 <skyliou520@gmail.com>
2014-08-16 15:48:50 +0800

    update NSURLConnectionDataDelegate

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLConnectionDataDelegate{
 
     
     var api: String = ""
@@ -16,19 +16,44 @@ class ViewController: UIViewController {
     @IBOutlet var city: UILabel!
     @IBOutlet var rainImage: UIImageView!
     
+    var data: NSMutableData = NSMutableData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.city.text = "I-Lan"
         var image = UIImage(named: "cloud.rain.png")
-        self.rainImage.image = image;
+        self.rainImage.image = image
         
         let background = UIImage(named: "oreo.jpg")
         
         self.view.backgroundColor = UIColor(patternImage: background)
+        
+        startConnection()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func startConnection(){
+        var restAPI: String = "http://api.openweathermap.org/data/2.5/weather?q=Taipei"
+        var url: NSURL = NSURL(string: restAPI)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: true)
+        println("start download")
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData dataReceived: NSData!) {
+        println("Downloading...")
+        
+        self.data.appendData(dataReceived)
+        
+       // NSString(data: self.data, encoding: NSUTF8StringEncoding)
+        println("current data is \( NSString(data: self.data, encoding: NSUTF8StringEncoding))")
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        println("download finished")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
